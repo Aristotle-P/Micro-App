156a10fb334e1df936574f630466aa3853c88540
Aristotle Poultney <aristotlepoultney@gmail.com>
2019-01-20 23:43:37 -0500

    fixed title

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "Micro-Blogger"
+    assert_equal full_title("Help"), "Help | Micro-Blogger"
   end
 end
